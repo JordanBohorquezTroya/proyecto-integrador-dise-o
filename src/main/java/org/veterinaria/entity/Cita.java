c5772376335b024e6c8b421ccca3822d959d6e6f
Jordan Troya <jordanbohorqueztroya@gmail.com>
2026-07-28 21:36:20 -0500

    Solucion - Estado de la cita como String

--- src/main/java/org/veterinaria/entity/Cita.java
+++ src/main/java/org/veterinaria/entity/Cita.java
@@ -1,10 +1,7 @@
 package org.veterinaria.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -21,7 +18,8 @@ public class Cita extends PanacheEntity {
 
     private String motivo;
 
-    private String estado;
+    @Enumerated(EnumType.STRING)
+    private EstadoCita estado;
 
     @ManyToOne
     @JoinColumn(name = "mascota_id")
@@ -30,5 +28,28 @@ public class Cita extends PanacheEntity {
     @ManyToOne
     @JoinColumn(name = "veterinario_id")
     private Veterinario veterinario;
+
+
+    public void confirmar() {
+        validarTransicion();
+        this.estado = EstadoCita.CONFIRMADA;
+    }
+
+    public void marcarComoAtendida() {
+        validarTransicion();
+        this.estado = EstadoCita.ATENDIDA;
+    }
+
+    public void cancelar() {
+        validarTransicion();
+        this.estado = EstadoCita.CANCELADA;
+    }
+
+    private void validarTransicion() {
+        if (estado == EstadoCita.ATENDIDA || estado == EstadoCita.CANCELADA) {
+            throw new IllegalStateException(
+                    "No se puede cambiar el estado de una cita que ya está " + estado);
+        }
+    }
 
 }
---
+++ src/main/java/org/veterinaria/entity/EstadoCita.java
@@ -0,0 +1,8 @@
+package org.veterinaria.entity;
+
+public enum EstadoCita {
+    PROGRAMADA,
+    CONFIRMADA,
+    ATENDIDA,
+    CANCELADA
+}
